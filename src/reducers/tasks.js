d5865ea95d62c18979f365943c2674a905aa517f
Micah Wood <micah@wpscholar.com>
2016-08-07 09:31:18 -0400

    Fix bug where action type was being set on tasks.

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -6,7 +6,7 @@ export function tasks(state = [], action) {
     switch (action.type) {
         case 'UPDATE_TASK':
             return function () {
-                const task = _.assign(state[action.index], _.omit(action, 'index'));
+                const task = _.assign(state[action.index], _.omit(action, 'type', 'index'));
                 return Immutable.fromJS(state).set(action.index, task).toJS();
             }();
         case 'DELETE_TASK':

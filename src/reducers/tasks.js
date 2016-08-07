c3e76dcadb8da25eebd7154bf47abeffafe7376e
Micah Wood <micah@wpscholar.com>
2016-08-07 09:32:43 -0400

    Added case for handling the 'CREATE_TASK'  action.

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -4,6 +4,11 @@ import _ from 'lodash'
 export function tasks(state = [], action) {
 
     switch (action.type) {
+        case 'CREATE_TASK':
+            return function () {
+                const index = _.findLastIndex(state) + 1;
+                return Immutable.fromJS(state).set(index, _.omit(action, 'type')).toJS();
+            }();
         case 'UPDATE_TASK':
             return function () {
                 const task = _.assign(state[action.index], _.omit(action, 'type', 'index'));

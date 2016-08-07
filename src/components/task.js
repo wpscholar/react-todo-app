6bc83d69da87b09affa007ba95755882302e12d2
Micah Wood <micah@wpscholar.com>
2016-08-06 21:19:27 -0400

    Added ability to switch into edit mode.

--- src/components/task.js
+++ src/components/task.js
@@ -1,25 +1,42 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
 import classNames from 'classnames'
 
 class Task extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false
+        };
+    }
 
     render() {
 
         const classes = {
             'task': true,
-            'completed': this.props.isComplete
+            'completed': this.props.isComplete,
+            'editing': this.state.isEditing
         };
 
         return (
             <div className={classNames(classes)}>
                 <input type="checkbox"/>
                 <label>
-                    <span>{this.props.name}</span>
-                    <input type="text" defaultValue={this.props.name}/>
+                    <span onDoubleClick={() => this.setState({isEditing: true})}>{this.props.name}</span>
+                    <input type="text" defaultValue={this.props.name} onBlur={() => this.setState({isEditing: false})}/>
                 </label>
                 <button type="button">Delete</button>
             </div>
         )
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.isEditing && !prevState.isEditing) {
+            const node = ReactDOM.findDOMNode(this).querySelector('input[type="text"]');
+            node.focus();
+            node.setSelectionRange(node.value.length, node.value.length);
+        }
     }
 
 }
